e3323297dd0b51104f7e715d6e81d8663534d1b9
Dave <dennehey@gmail.com>
2024-08-29 16:20:14 -0700

    It renames your saves to be sequential in number so they can get picked up by the autorecord script correctly

---
+++ Scripts/renameSavestoSequentialIntegerFromHex.ps1
@@ -0,0 +1,13 @@
+[string]$rootPathForFiles = 
+[string[]]$listOfFilesToRename = Get-ChildItem -Path $rootPathForFiles -Filter '*.save' | Select-Object -ExpandProperty FullName
+$listOfFilesToRename | ForEach-Object {
+    [string]$newName = Split-Path -Path $_ -Leaf 
+    
+    $newName = $newName -replace '^(\w+)-([0-9a-fA-F]+)-(\w+)-0-([0-9a-fA-F]+)[(.save)]', '$2'    
+    Write-Verbose $newName -Verbose
+    $newName = $newName.Replace('.save', '')
+    $newName = $newName.Replace('save', '')
+    $newName = [System.Convert]::ToInt32($newName,16)
+    Write-Verbose $newName -Verbose
+    Rename-Item -Path $_ -NewName $newName".save"
+}
