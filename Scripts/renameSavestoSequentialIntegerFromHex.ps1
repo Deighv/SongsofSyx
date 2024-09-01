b752018fa6d73dc9b34018ee34e94beceb4bd1db
Dave <dennehey@gmail.com>
2024-09-01 04:34:56 -0700

    Update renameSavestoSequentialIntegerFromHex.ps1

--- Scripts/renameSavestoSequentialIntegerFromHex.ps1
+++ Scripts/renameSavestoSequentialIntegerFromHex.ps1
@@ -6,7 +6,7 @@ $listOfFilesToRename | ForEach-Object {
     $newName = $newName -replace '^(\w+)-([0-9a-fA-F]+)-(\w+)-0-([0-9a-fA-F]+)[(.save)]', '$2'    
     Write-Verbose $newName -Verbose
     $newName = $newName.Replace('.save', '')
-    $newName = $newName.Replace('save', '')
+    $newName = $newName.Replace('save', '') #I know I know
     $newName = [System.Convert]::ToInt32($newName,16)
     Write-Verbose $newName -Verbose
     Rename-Item -Path $_ -NewName $newName".save"
