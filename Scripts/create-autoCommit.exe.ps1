99eefd9bad58f91510156f3c28a922f078eeda19
Dave <dennehey@gmail.com>
2024-09-01 04:34:30 -0700

    Update create-autoCommit.exe.ps1

--- Scripts/create-autoCommit.exe.ps1
+++ Scripts/create-autoCommit.exe.ps1
@@ -1,2 +1,2 @@
 Install-Module -Name ps2exe
-Invoke-PS2EXE .\autoCommit.ps1 .\TwitchTvDeighv_.exe -title 'TwitchTvDeighv_' -description 'TwitchTvDeighv_'
+Invoke-PS2EXE .\autoCommit.ps1 .\AutoCommit.exe
