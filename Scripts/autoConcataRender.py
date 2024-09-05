04abc73543abc42bf7aea6cea55afb840f694e4e
Dave <dennehey@gmail.com>
2024-09-05 02:03:22 -0700

    This runs much better now

--- Scripts/autoConcataRender.py
+++ Scripts/autoConcataRender.py
@@ -1,49 +1,53 @@
-import argparse
 import subprocess
 import itertools
 import datetime
-import time
-import shutil
 import math
-import decimal
 from os import walk
 import ffmpeg
-import os
+import numpy as np
 
-Fade_Rectifier=1
+fade_duration=1.0
 
-folderWithRecordings = "C:\\RecordingsSendIt\\" #this is just the folder we're in really..
+folderWithRecordings = "C:\\RecordingsSendIt\\iMissedTheLastVideo\\" #this is just the folder we're in really..
 filenamesToStart = next(walk(folderWithRecordings), (None, None, []))[2]  
 filenamesToStart = [fi for fi in filenamesToStart if fi.endswith(".mkv")] #just the videos
 filenamesToStart = [fi for fi in filenamesToStart if not fi.startswith("output")] #not our outputs
-runInBatchesOf = int(15) #many Filters uses many RAMs, adjust this until you're using just under your max ram, varies with clip size
+runInBatchesOf = int(8) #many Filters uses many RAMs, adjust this until you're using just under your max ram, varies with clip size
 NumberOfClips =  len(filenamesToStart)
 NumberOfRuns = math.floor(NumberOfClips/runInBatchesOf)
 
 print(NumberOfRuns)
 index = 0
-while index <= NumberOfRuns: #This will overflow and I know
+while index <= NumberOfRuns: #This can overflow and I know
     
-    output_filename = "output"+str(datetime.datetime.now())+".mkv" #name our output file
+    output_filename = "readypls.mkv" #name our output file
     
     fromThis = int((runInBatchesOf*index)) #The number file in the group we want to start at
     toThis = int(fromThis+runInBatchesOf) #The number file in the group we want to end at
     ourFiles = []
     for x in range(fromThis, toThis):
         print(filenamesToStart[x])
-        ourFiles += [filenamesToStart[x]]
+        ourFiles += [filenamesToStart[x]] 
+        
     with open('render.log', 'a') as f:
         print(output_filename, file=f)
         print(ourFiles, file=f)
         print(datetime.datetime.now(), file=f)
         print("----------------------------------------------------", file=f)
     segments = ourFiles
-    #thanks https://gist.github.com/royshil/369e175960718b5a03e40f279b131788.. although it's still not quite right? Need to just redo from scratch
+    print(ourFiles)
+    #thanks https://gist.github.com/royshil/369e175960718b5a03e40f279b131788
+    
     # Get the lengths of the videos in seconds
-    file_lengths = [
-        float(ffmpeg.probe(f)["format"]["duration"])
-        for f in segments
-    ]
+
+    file_lengths = []
+    for x in range(fromThis, toThis):
+        file = folderWithRecordings+filenamesToStart[x]
+        video_length = ffmpeg.probe(file)["format"]["duration"]
+        #video_length = float(video_length)
+        file_lengths += [video_length]
+    print(file_lengths)
+    file_lengths = list(np.float64(file_lengths))
 
     width = 3840
     height = 2160
@@ -54,43 +58,36 @@ while index <= NumberOfRuns: #This will overflow and I know
     # Prepare the filter graph
     video_fades = ""
     audio_fades = ""
-    last_fade_output = "0v"
+    last_fade_output = "0:v"
     last_audio_output = "0:a"
     video_length = 0
-    normalizer = ""
-    scaler_default = f",scale=w={width}:h={height}:force_original_aspect_ratio=1,pad={width}:{height}:(ow-iw)/2:(oh-ih)/2"
+    fade_duration = 1.0
 
-    for i in range(len(segments)):
-        """     # Videos normalizer
-        scaler = scaler_default if i > 0 else ""
-        normalizer += f"[{i}:v]settb=AVTB,setsar=sar=1,fps=60{scaler}[{i}v];" """
+    for i in range(len(segments) - 1):
+        # Video graph: chain the xfade operator together
+        video_length += file_lengths[i]
+        next_fade_output = "v%d%d" % (i, i + 1)
+        video_fades += "[%s][%d:v]xfade=duration=1.0:offset=%.3f[%s]; " % \
+            (last_fade_output, i + 1, video_length - fade_duration*(i+1), next_fade_output)
+        last_fade_output = next_fade_output
 
-        if i == 0:
-            continue
-
-        #Video graph: chain the xfade operator together
-        video_length += file_lengths[i - 1] - Fade_Rectifier
-        next_fade_output = "v%d%d" % (i - 1, i)
-        video_fades += "[%s][%dv]xfade=duration=%d:offset=%.5f[%s];" % (last_fade_output, i, Fade_Rectifier, video_length, next_fade_output)
-        last_fade_output = next_fade_output #the .5 above is the video timing
-
-        #Audio graph:
-        next_audio_output = "a%d%d" % (i - 1, i)
-        audio_fades += f"[{last_audio_output}][{i}:a]acrossfade=d=%d[{next_audio_output}];" % (Fade_Rectifier)
+        # Audio graph:
+        next_audio_output = "a%d%d" % (i, i + 1)
+        audio_fades += "[%s][%d:a]acrossfade=d=1[%s]%s " % \
+            (last_audio_output, i + 1, next_audio_output, ";" if (i+1) < len(segments)-1 else "")
         last_audio_output = next_audio_output
-
-    video_fades += f"[{last_fade_output}]format=pix_fmts=yuv420p[final];"
 
     # Assemble the FFMPEG command arguments
     ffmpeg_args = ['ffmpeg',
                 *itertools.chain(*files_input),
-                '-filter_complex', normalizer + video_fades + audio_fades[:-1], #this is the audio timing
-                '-map', '[final]',
-                '-map', f"[{last_audio_output}]",
+                '-filter_complex', video_fades + audio_fades,
+                '-map', '[%s]' % last_fade_output,
+                '-map', '[%s]' % last_audio_output,
                 '-y',
                 output_filename]
 
-    print(" ".join(ffmpeg_args))
     # Run FFMPEG
+    print(" ".join(ffmpeg_args))
     subprocess.run(ffmpeg_args)
-    index +=1 
+    index +=1
+    exit()
